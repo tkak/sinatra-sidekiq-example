965085eb7f741aff625ef03a6486374bb7dcb620
takaaki.furukawa <takaaki.furukawa@rakuten.com>
2015-12-28 22:19:55 +0900

    Add docker-compose.yml

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require 'sidekiq'
 require 'sinatra'
 
-get '/' do
-  p 'hello world'
+#Sidekiq.configure_server do |config|
+#  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" }
+#end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" }
 end
+
+#$redis = Redis.new()
+
+class SomeWorker
+  include Sidekiq::Worker
+  def perform(id)
+    p id
+  end
+end
+
+get '/things' do
+  p 'hoge' 
+end
+
+post '/things' do
+  SomeWorker.perform_async(1)
+  redirect to('/things')
+end
+
