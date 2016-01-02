1708ebb68de949fe9931faca1b386c768fbcb3b5
takaaki.furukawa <takaaki.furukawa@rakuten.com>
2016-01-03 00:36:49 +0900

    Tweak

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sidekiq'
 require 'sinatra/base'
-require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}" }
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require './app'
+require 'sidekiq/web'
 
 run Rack::URLMap.new('/' => SomeApp, '/sidekiq' => Sidekiq::Web)
